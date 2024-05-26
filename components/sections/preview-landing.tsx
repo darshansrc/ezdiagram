c5e6b9b4635aec1568fe8767ac8f789abe92e866
darshan-sr <darshansr1618@gmail.com>
2024-05-27 02:33:53 +0530

    chore: Update landing page video to use demo.mp4

--- components/sections/preview-landing.tsx
+++ components/sections/preview-landing.tsx
@@ -6,14 +6,17 @@ export function PreviewLanding() {
       <div className="container max-w-7xl">
         <div className="rounded-xl md:bg-muted/30 md:p-3.5 md:ring-1 md:ring-inset md:ring-border">
           <div className="relative aspect-video overflow-hidden rounded-xl border md:rounded-lg">
-            <Image
+            <video
               className="size-full object-cover object-center"
-              src="/images/landing.png"
-              alt="preview landing"
+              src="/videos/demo.mp4"
               width={2000}
               height={1000}
-              priority={true}
-            />
+              controls
+              autoPlay
+              loop
+            >
+              <source src="/videos/demo.mp4" type="video/mp4" />
+            </video>
           </div>
         </div>
       </div>
